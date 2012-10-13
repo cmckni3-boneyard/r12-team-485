14cca8375d6ac3fed10d96b202492af8acffbb83
Chris McKnight <cmckni3@gmail.com>
2012-10-13 17:26:26 -0500

    Added some flash helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+    def nav_link(link_text, link_path)
+        class_name = current_page?(link_path) ? 'active' : ''
+        
+        content_tag(:li, :class => class_name) do
+            link_to link_text, link_path
+        end
+    end
+
+    def flash_type_helper(name)
+        case name
+        when :notice
+            "success"
+        when :error
+            "error"
+        else
+            "warning"
+        end
+    end
+
 end
